5e09a13b588e06e9402c20fd972c535102ae41d6
Michael Dilger <MichaelDilger@Michaels-MacBook-Pro.local>
2017-05-04 13:22:17 +0200

    Initial Commit

---
+++ Google Docs/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Google Docs
+//
+//  Created by Michael Dilger on 04.05.17.
+//  Copyright © 2017 Michael Dilger. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Google Docs/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Google Docs
+//
+//  Created by Michael Dilger on 04.05.17.
+//  Copyright © 2017 Michael Dilger. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
